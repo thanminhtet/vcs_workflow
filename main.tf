405b767e9dfc07d65aef22ecfccf6400b93d6573
thanminhtet <thanminhtetttt@gmail.com>
2025-02-17 11:44:47 +0630

    toetoe

--- main.tf
+++ main.tf
@@ -8,3 +8,14 @@ resource "aws_iam_user" "demo_user" {
   }
 
 }
+
+resource "aws_iam_user" "toetoe" {
+  name = "toetoe-1"
+  path = "/"
+
+  tags = {
+    environment = "Development"
+    created_by  = "Terraform"
+  }
+
+}
